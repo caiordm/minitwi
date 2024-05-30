d64f7da64c2229d241c65cd40ac689187ce4a187
caiordm <caiordm08@gmail.com>
2024-05-30 13:31:38 -0300

    feat: updating pagination style

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
     def index
-        @tweets = Tweet.paginate(:page => params[:page], per_page: 5)
+        @tweets = Tweet.paginate(:page => params[:page], per_page: 5).order('created_at DESC')
     end
 end
