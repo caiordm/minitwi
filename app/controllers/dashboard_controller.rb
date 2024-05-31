8a1cb4e3fa038393626f55812c0899b7910e396e
caiordm <caiordm08@gmail.com>
2024-05-31 13:25:06 -0300

    feat: full pagination

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
     before_action :authenticate_user!
     
     def index
-
+        @tweets = Tweet.paginate(:page => params[:page], per_page: 20).order('created_at DESC')
     end
 end
